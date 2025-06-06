9b17beef5e37c7a43d82c20e63c1e527c8f2c341
tri <tri@wiserobot.com>
2025-06-06 16:39:35 +0700

    add feature Authentication with JWT

---
+++ src/routes/auth/auth.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { register, login } from '../../controllers/auth/auth.controller';
+import { validateForm } from '../../middleware/auth/validation.middleware';
+
+const router = Router();
+
+router.post('/register', validateForm, register);
+router.post('/login', validateForm, login);
+
+export default router; 
---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import authRoutes from './auth/auth.routes';
+
+const router = Router();
+
+// Auth routes
+router.use('/auth', authRoutes);
+
+export default router; 
